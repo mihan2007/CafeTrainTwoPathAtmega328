1d9f0bb0cec5d285114baf76e2a489d1230c03cf
mihan2007 <mihan2007@yandex.ru>
2025-02-27 00:42:02 +0300

    Uart

--- CafeTrainAtmega328/main.cpp
+++ CafeTrainAtmega328/main.cpp
@@ -10,41 +10,62 @@
 #include "routes.h"
 #include "PWM.h"
 
-char firstLineText[16]; // Text for the first line
-char secondLineText[16]; // Text for the second line
+#define BAUD 9600
+#define UBRR_VALUE ((unsigned long)(F_CPU / 16 / BAUD) - 1)
+
+void UART_init(void) {
+	UBRR0H = (UBRR_VALUE >> 8);
+	UBRR0L = UBRR_VALUE;
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void UART_send(char data) {
+	while (!(UCSR0A & (1 << UDRE0)));
+	UDR0 = data;
+}
+
+char UART_receive(void) {
+	while (!(UCSR0A & (1 << RXC0)));
+	return UDR0;
+}
+
+char firstLineText[16]; // Глобальные строки
+char secondLineText[16];
 
 int main(void)
 {
-	// Module initialization
+	// Инициализация модулей
 	I2C_Init();
-	LCD_Init();	
+	LCD_Init();
 	LCD_Clear();
 	
+	UART_init();
 	initRailRoadSwitch();
+	//initPWM();
 	
-	initPWM();
-	
-    char firstLineText[20];
-    char secondLineText[20];
+	while (1) {
+		char received;
+		
+		if (UCSR0A & (1 << RXC0)) {  // Проверяем, есть ли данные
+			received = UART_receive();
+			} else {
+			received = '-';  // Если данных нет, записываем прочерк
+		}
 
-	
-    while (1) {
-        /*snprintf(firstLineText, sizeof(firstLineText), "Table" );
-        snprintf(secondLineText, sizeof(secondLineText),"Right");
+		
+		snprintf(firstLineText, sizeof(firstLineText), "Control Panel" );
+		snprintf(secondLineText, sizeof(secondLineText), "%c", received);
 
-        LCD_PrintTwoLines(firstLineText, secondLineText, 0);
-        _delay_ms(300);*/
-		LocomotiveSpeedUp();
+		LCD_PrintTwoLines(firstLineText, secondLineText, 0);
+		_delay_ms(300);
+		
+		/*LocomotiveSpeedUp();
 		moveLocomotive(1);
 		_delay_ms(5000);
 		stopLocomotive();
-		_delay_ms(5000);				
-        }
+		_delay_ms(5000);*/
+	}
 	
-    return 0;
+	return 0;
 }
-
-
-
-
-
